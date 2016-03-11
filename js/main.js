023db7b5b2293a78c611b0ae4c08ef487d95d28e
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-11 10:16:23 -0500

    YAAAAAAAHS

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $h1Above = $('<h1>');
+var $h1Below = $('<h1>');
+var $ulA = $('<ul>');
+var $ulB = $('<ul>');
+
+$h1Above.html('Below ground veggies');
+$h1Below.html('Above ground veggies');
+
+$('body').append($h1Above);
+$('body').append($ulA);
+
+$('body').append($h1Below);
+$('body').append($ulB);
+
+$('ul li').each(function() {
+  if($(this).hasClass('above')){
+    $ulA.append($(this))
+  } else {
+    $ulB.append($(this))
+  }
+});
